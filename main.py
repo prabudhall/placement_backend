b2f608e8189ce2c24e36a6d447d9db9f51e6a600
Prabudh <prabudh2002@gmail.com>
2024-02-27 00:44:47 +0530

    Added Skeleton

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from fastapi import FastAPI
 
+from cn_subject import cn
+from dbms_subject import dbms
+from os_subject import os
+
 app = FastAPI()
+
+app.include_router(cn.router,
+                   prefix="/cn",
+                   tags=["Computer Networks"])
+
+app.include_router(dbms.router,
+                   prefix="/dbms",
+                   tags=["DBMS"])
+
+app.include_router(os.router,
+                   prefix="/os",
+                   tags=["OS"])
 
 @app.get("/")
 async def root():
